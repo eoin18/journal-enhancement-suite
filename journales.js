ff66ba358fd5a5199d9e33ad2262c622040e4573
eoin18 <eoin1818@gmail.com>
2021-03-08 13:58:47 +0000

    Also hide the comments count

--- journales.js
+++ journales.js
@@ -1,2 +1,3 @@
 document.querySelector("#comments").hidden = true
 document.querySelector("#respond").hidden = true
+document.querySelectorAll("a[href*='/#comments']").forEach(element => element.hidden=true)
